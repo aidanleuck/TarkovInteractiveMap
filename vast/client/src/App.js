f724ce24978729f922937f7681241f59789fe702
Sunny Moran <66544279+sunnymoran@users.noreply.github.com>
2021-10-31 14:50:25 -0600

    Closes #23 (#30)

--- vast/client/src/App.js
+++ vast/client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { ViewToolBar } from './components/Toolbar.js';
 import {ViewController} from "./components/ViewController.js"
+import { Marker, ViewMarker } from './components/Marker.js';
 import image from './Images/customsMap.jpg'
 
 function App() {
- 
  // return ViewController({image});
  return ViewToolBar();
 }
---
+++ vast/client/src/components/Marker.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../CSS/marker.css'
+
+export const ViewMarker = () => {
+    return(   
+
+<div class="main">
+<h1><button type="button" onclick="">Enable All</button> <button type="button" onclick="">Disable All</button> Markers</h1>
+<p><button type="button" onclick="">Loose Loot</button> <button type="button" onclick="">Jacket</button> <button type="button" onclick="">Locked Door</button> <button type="button" onclick="">Extraction</button>
+  <button type="button" onclick="">Dead Scav</button> <button type="button" onclick="">Filing Cabinet</button>
+  <button type="button" onclick="">Safe</button> <button type="button" onclick="">Duffle Bag</button></p>
+<div>
+<div></div>
+<button type="button" onclick="">Armor</button> <button type="button" onclick="">Crate</button> <button type="button" onclick="">Location</button> <button type="button" onclick="">Miscellaneous</button>
+  <button type="button" onclick="">Money</button> <button type="button" onclick="">Provisions</button>
+  <button type="button" onclick="">Valuable</button> <button type="button" onclick="">Grenade</button>
+<div></div>
+<button type="button" onclick="">Key</button> <button type="button" onclick="">Weapon Box</button> <button type="button" onclick="">Quest Item</button> <button type="button" onclick="">Spawn Point</button> <button type="button" onclick="">MEDS</button> <button type="button" onclick="">Weapon Mods</button> <button type="button" onclick="">Boss</button> <button type="button" onclick="">Loose Loot</button></div>
+</div>
+    );
+}
