4bd997914a6e5407554e5f06893fbc9be772390a
Sunny Moran <66544279+sunnymoran@users.noreply.github.com>
2021-10-31 14:46:36 -0600

    Closes #24 (#29)

--- vast/client/src/App.js
+++ vast/client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { ViewToolBar } from './components/Toolbar.js';
 import {ViewController} from "./components/ViewController.js"
 import image from './Images/customsMap.jpg'
 
 function App() {
  
-  return ViewController({image});
+ // return ViewController({image});
+ return ViewToolBar();
 }
  
 export default App
---
+++ vast/client/src/components/Toolbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../CSS/toolbar.css'
+
+
+export const ViewToolBar = () => {
+
+return(
+
+<html>
+<head>
+</head>
+<body>
+<button type="button">Back to center</button>
+<div></div>
+<button type="button">Floor Level</button>
+<div></div>
+<div></div>
+<button type="button">Different Map Versions</button> 
+<div></div> 
+<h2>Progress Tracker</h2>
+<div>
+<button type="button">Sign in</button>
+</div>
+<ul>
+  <li><a href="#Quest 1">Quest 1</a></li>
+  <li><a href="#Quest 2">Quest 2</a></li>
+  <li><a href="#Quest 3">Quest 3</a></li>
+  <li><a href="#Quest 4">Quest 4</a></li>
+  <li><a href="#Quest 5">Quest 5</a></li>
+  <li><a href="#Quest 6">Quest 6</a></li>
+  <li><a href="#Quest 7">Quest 7</a></li>  
+</ul>
+</body>
+</html>
+    );
+
+}
+
