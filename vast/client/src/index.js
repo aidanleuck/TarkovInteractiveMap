dfe3480d3b87426f2ace6c1f8a70e1372d9d0f77
Aidan Leuck <48965785+aidanleuck@users.noreply.github.com>
2021-11-10 12:32:39 -0700

    Closes #49 (#50)

--- vast/client/src/index.js
+++ vast/client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./CSS/main.css"
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
