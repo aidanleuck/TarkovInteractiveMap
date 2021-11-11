76b067f08930fe95c50ce1b890b4e7ef5442320f
vmcerda <47342925+vmcerda@users.noreply.github.com>
2021-11-10 20:18:11 -0700

    Closes #45 (#52)

--- vast/server/server.js
+++ vast/server/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+const { Pool } = require('pg')
 const express = require('express'); //Line 1
 const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+const port = process.env.PORT || 8000; //Line 3
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
@@ -8,4 +10,16 @@ app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //Line 11
+});
+
+// pools will use environment variables
+// for connection information
+const pool = new Pool({
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
