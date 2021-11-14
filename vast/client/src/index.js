fc7c0f62d70f97d619ab815be6601e5324af8865
Aidan Leuck <48965785+aidanleuck@users.noreply.github.com>
2021-11-14 12:55:40 -0700

    Closes #44 (#54)

--- vast/client/src/index.js
+++ vast/client/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./CSS/main.css"
 import 'bootstrap/dist/css/bootstrap.css';
+import "./CSS/main.css"
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,3 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+<script src="https://kit.fontawesome.com/a14d658131.js" crossorigin="anonymous"></script>
