93ddec781aaadd132a15e229f338cd0d7ab4ea6d
Sunny Moran <66544279+sunnymoran@users.noreply.github.com>
2021-11-14 10:49:22 -0700

    Closes #47 (#55)

--- vast/client/src/App.js
+++ vast/client/src/App.js
@@ -7,7 +7,7 @@ import {Header} from "./components/Header.js"
 function App() {
   
  // return ViewController({image});
-    return ViewMarker();
+    return ViewToolBar();
      
 }
  
--- vast/client/src/components/Toolbar.js
+++ vast/client/src/components/Toolbar.js
@@ -6,33 +6,35 @@ export const ViewToolBar = () => {
 
 return(
 
-<html>
-<head>
-</head>
-<body>
-<button type="button">Back to center</button>
-<div></div>
-<button type="button">Floor Level</button>
-<div></div>
-<div></div>
-<button type="button">Different Map Versions</button> 
-<div></div> 
-<h2>Progress Tracker</h2>
-<div>
-<button type="button">Sign in</button>
-</div>
-<ul>
-  <li><a href="#Quest 1">Quest 1</a></li>
-  <li><a href="#Quest 2">Quest 2</a></li>
-  <li><a href="#Quest 3">Quest 3</a></li>
-  <li><a href="#Quest 4">Quest 4</a></li>
-  <li><a href="#Quest 5">Quest 5</a></li>
-  <li><a href="#Quest 6">Quest 6</a></li>
-  <li><a href="#Quest 7">Quest 7</a></li>  
-</ul>
-</body>
-</html>
-    );
+  <div class="container-fluid">
+    <div class="buttons row justify-content-center">
+      <div class="col-md-6">
+        <div class="col-3"><button type="button" class="btn btn-dark" onclick="">Back to center</button></div>
+        <div class="col-3"><button type="button" class="btn btn-dark" onclick="">Floor Level</button></div>
+        <div class="col-3"><button type="button" class="btn btn-dark" onclick="">Different Map Versions</button></div>
+        <div class="col-3"><h1>Progress Tracker</h1></div>
+        <div class="col-3"><button type="button" class="btn btn-dark" onclick="">Sign in</button></div>
+        <div class="col-3">
+          <ul>
+            <li><a href="#Quest 1">Quest 1</a></li>
+            <li><a href="#Quest 2">Quest 2</a></li>
+            <li><a href="#Quest 3">Quest 3</a></li>
+            <li><a href="#Quest 4">Quest 4</a></li>
+            <li><a href="#Quest 5">Quest 5</a></li>
+            <li><a href="#Quest 6">Quest 6</a></li>
+            <li><a href="#Quest 7">Quest 7</a></li>
+          </ul>
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
+
+
+  );
 
 }
 
