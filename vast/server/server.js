aefc63e4d435da8dd00acba998b5c23b296d5281
vmcerda <47342925+vmcerda@users.noreply.github.com>
2021-12-05 20:30:23 -0700

    Closes #61 (#69)

--- vast/server/server.js
+++ vast/server/server.js
@@ -4,14 +4,6 @@ const express = require('express'); //Line 1
 const app = express(); //Line 2
 const port = process.env.PORT || 8000; //Line 3
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
-
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-});
-
 // pools will use environment variables
 // for connection information
 const pool = new Pool({
@@ -19,7 +11,17 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 })
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+
+function validate(username,id){
+  pool.query('SELECT username, last_name FROM users where username = $1 and user_id = $2',[username,id],function(err,result){
+    if(err || result.rowCount == 0){
+      console.log("False");
+    } else{
+      console.log("true")
+      console.log(result.rows)
+    }
+  });
+}
+
+validate("aidan", 1);
+validate("taylor", 1);
